d15908eecb9fdd4d3be201e15bea0b34cc5538b4
olhanotolga <olha.halat@gmail.com>
2025-12-11 17:17:42 +0100

    feat: add avl balanced tree

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -22,17 +22,140 @@
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      this.root.add(value);
+    }
+  }
+
+  toObject() {
+    return this.root;
+  }
 }
 
 class Node {
-  // code also goes here
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.height = 1;
+  }
+
+  add(value) {
+    // decide to go left or right (call .add() on the left or right child)
+    if (value < this.value) {
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || (this.left && this.right.height < this.left.height)) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
+
+    this.balance();
+  }
+
+  balance() {
+    // check if this node is out of balance (by the difference between left and right heights)
+    const rightHeight = this.right ? this.right.height : 0;
+    const leftHeight = this.left ? this.left.height : 0;
+
+    // if it's out of balance, decide of signle or double rotation
+    // if single - call rotate on self,
+    // if double - call rotate on child, and then on self
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left?.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left?.left ? this.left.left.height : 0;
+
+      // the subtree is "bent" => double rotation:
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      this.rotateLL();
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right?.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right?.left ? this.right.left.height : 0;
+
+      // the subtree is "bent" => double rotation:
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      this.rotateRR();
+    }
+  }
+
+  // called if the right child is heavy
+  rotateRR() {
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
+
+    this.left.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  // called if the left child is heavy
+  rotateLL() {
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+
+    this.right.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  updateInNewLocation() {
+    // recalculate height
+    if (!this.left && !this.right) {
+      this.height = 1;
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1;
+    } else {
+      this.height = this.right.height + 1;
+    }
+  }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("AVL Tree", function () {
-  test("creates a correct tree", () => {
+describe('AVL Tree', function () {
+  test('creates a correct tree', () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
     nums.map((num) => tree.add(num));
