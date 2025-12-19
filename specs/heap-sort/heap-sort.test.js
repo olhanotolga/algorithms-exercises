e60ae1d9d01186514de2ddef7f92bdafdd4096c3
olhanotolga <olha.halat@gmail.com>
2025-12-19 14:40:12 +0100

    feat: add heap sort

--- specs/heap-sort/heap-sort.test.js
+++ specs/heap-sort/heap-sort.test.js
@@ -7,17 +7,50 @@
 */
 
 const heapSort = (array) => {
-  // code
+  array = createMaxHeap(array);
+
+  for (let i = array.length - 1; i > 0; i--) {
+    swapPlace(0, i, array);
+    heapify(array, 0, i);
+  }
+
   return array;
 };
 
 const createMaxHeap = (array) => {
-  // code
+  for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+    heapify(array, i, array.length);
+  }
+  return array;
 };
 
 const heapify = (array, index, heapSize) => {
-  // code
+  const leftChildIdx = 2 * index + 1;
+  const rightChildIdx = 2 * index + 2;
+
+  let largestValIdx = index;
+  
+  // check if out of bounds AND if needs swapping
+  if (leftChildIdx < heapSize && array[largestValIdx] < array[leftChildIdx]) {
+    largestValIdx = leftChildIdx;
+  }
+  if (rightChildIdx < heapSize && array[largestValIdx] < array[rightChildIdx]) {
+    largestValIdx = rightChildIdx;
+  }
+
+  if (largestValIdx !== index) {
+    swapPlace(index, largestValIdx, array);
+    heapify(array, largestValIdx, heapSize);
+  }
 };
+
+const swapPlace = (index1, index2, array) => {
+  const temp = array[index1];
+  array[index1] = array[index2];
+  array[index2] = temp;
+
+  return array;
+}
 
 // unit tests
 // do not modify the below code
